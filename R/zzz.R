39d7428454094c47ec07e727abc13361c80e5599
Simon Gillings <simon.gillings@bto.org>
2018-09-21 16:38:50 +0100

    update package name in onload function

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 #' Initialise the package including detect environment and load default paths
-.onLoad<-function(libname=find.package('birdatlas'),pkgname='birdatlas') {
-  cat("Birdatlas version 0.1.1 - access and manipulate bird atlas data")
+.onLoad<-function(libname=find.package('BTOTools'),pkgname='BTOTools') {
+  cat("BTOTools version 0.1.0 - access and manipulate BTO data")
   if(.Platform$OS.type =='windows') assign('.BTOarchive_path','//btodomain/FILES/UNIXArchive/',1)
   if(.Platform$OS.type=='unix') assign('.BTOarchive_path','/archive/',1)
 }
