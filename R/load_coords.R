8303bd78a985a678482eaf5971009fd893e1d8bd
Simon Gillings <38919042+btosimongillings@users.noreply.github.com>
2018-10-12 16:32:54 +0100

    merge new code (#3)
    * initialise fork
    
    * Minor format and documentation improvements to test fork and pull request process
    
    * corrected documentation and added error check for passed function parameter
    
    * new prog to load open access data files

--- R/load_coords.R
+++ R/load_coords.R
@@ -4,6 +4,8 @@
 #' Chose to load coordinates for tetrads, 10-km, 20-km, 50-km or 100-km squares. Note that coordinates
 #' for the Channel Isles have been moved to where they were mapped in the Atlas book.
 #'
+#' @param res numeric resolution of the coordinates required: 2, 10, 20, 50 or 100
+#'
 #' @return A dataframe with the following columns:
 #' \item{tenkm}{10-km square, or tetrad_id, segref, quadref, hundref depending on chosen grid resolution}
 #' \item{easting}{British National grid easting of square centroid}
@@ -14,6 +16,9 @@
 #'
 #' @export
 load_coords<-function(res) {
+  #errorcheck
+  if(!res %in% c(2, 10, 20, 50, 100)) stop('Resolution must be one of: 2, 10, 20, 50, 100')
+  
   #convert input resolution to text for file name
   res<-ifelse(res==2,'002',res)
   res<-ifelse(res==10,'010',res)
@@ -39,3 +44,4 @@ load_coords<-function(res) {
   names(df)<-tolower(names(df))
   return(df)
 }
+
