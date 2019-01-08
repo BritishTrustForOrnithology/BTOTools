5838875388ebb7db80defdc2c0d1728afd27b628
Simon Gillings <simon.gillings@bto.org>
2019-01-08 09:28:38 +0000

    corrected encoding of some accented characters

--- data-raw/make_included_datafiles.R
+++ data-raw/make_included_datafiles.R
@@ -2,7 +2,7 @@ library(devtools)
 #' dataframe of species scientific name synonyms
 
 #' read the old scientific names with their matched new names
-sciname_synonyms <- read.csv('data-raw/sci_name_synonyms.csv', colClasses = c('numeric','character','character','numeric'))
+sciname_synonyms <- read.csv('data-raw/sci_name_synonyms.csv', colClasses = c('numeric','character','character','numeric'), encoding = 'latin1')
 names(sciname_synonyms) <- tolower(names(sciname_synonyms))
 
 #remove a couple of NA type values
@@ -10,15 +10,17 @@ sciname_synonyms <- subset(sciname_synonyms, master_taxon_id != 0)
 sciname_synonyms <- subset(sciname_synonyms, alternative_sci_name != 'n/a')
 
 #output
-use_data(sciname_synonyms)
+use_data(sciname_synonyms, overwrite = TRUE)
 
 
 #' dataframe of species names for all birds (globally) plus selected UK mammals, herps, inverts
 global_species_lookup <- read.csv('data-raw/global_species_lookup.csv', 
-                                  colClasses = c('numeric', rep('character', 2), 'numeric', 'character','numeric', 'character', 'character' ))
+                                  colClasses = c('numeric', rep('character', 2), 'numeric', 'character','numeric', 'character', 'character' ), encoding = 'latin1')
 names(global_species_lookup) <- tolower(names(global_species_lookup))
 # head(global_species_lookup)
 # str(global_species_lookup)
+#check the encoding of an accented character
+#subset(global_species_lookup, master_taxon_id == 405)
 
 #infill NA values for cbc_code
 global_species_lookup$cbc_code <- ifelse(global_species_lookup$cbc_code == '', NA, global_species_lookup$cbc_code)
