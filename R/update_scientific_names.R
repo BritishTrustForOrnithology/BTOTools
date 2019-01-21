1ccb752b48501bacb6471cd22a8492aae141b70f
Simon Gillings <simon.gillings@bto.org>
2019-01-21 10:03:34 +0000

    Added option to report which species have changed to the screen

--- R/update_scientific_names.R
+++ R/update_scientific_names.R
@@ -8,8 +8,12 @@
 #'
 #' @param df character The name of the dataset to be scanned for name updates
 #' @param var character A quoted string. The name of the column in df to be scanned for name updates
+#' @param report_changes logical Should the list of names changed be displayed on execution (default FALSE)
 #' 
-#' @return A dataframe
+#' @return A dataframe.
+#' 
+#' @details If \code{report_changes=TRUE} the list of cases changed is printed to output, showing original
+#' name (from), destination name (to) and the number of rows affected.
 #'
 #' @seealso \code{\link{sciname_synonyms}}
 #'
@@ -21,10 +25,11 @@
 #'                                                var = 'sciname')
 #'
 #' @export
-update_scientific_names <- function(df, var) {
+update_scientific_names <- function(df, var, report_changes=FALSE) {
   #errorcheck
   if(!(is.data.frame(df))) stop('df must be an existing dataframe')
   if(!var %in% names(df)) stop('var is not a column name in df')
+  if(!is.logical(report_changes)) stop('report_changes must be TRUE or FALSE')
   
   #check which variable holds the scientific names to be scanned
   varn <- which(names(df) == var)
@@ -43,6 +48,12 @@ update_scientific_names <- function(df, var) {
   num_recs_in_df <- (nrow(df))
   num_recs_updated_in_df <- (nrow(df[!is.na(df$scinameworking2),]))
   cat(num_recs_updated_in_df, ' out of ', num_recs_in_df, ' records updated.')
+  
+  #optional reporting
+  if(report_changes == TRUE) {
+    print(setNames(aggregate(data = temp, unit ~ scinameworking + scinameworking2, NROW), c('from', 'to', 'num_rows')))
+  }
+  
   #copy across the unchanged scinames
   df$scinameworking2 <- ifelse(is.na(df$scinameworking2), df$scinameworking, df$scinameworking2)
   #copy this new list of scinames overwriting the original column
