03a029dbc0e65e316f453e341cb3eb70332540cf
Simon Gillings <simon.gillings@bto.org>
2019-01-22 14:02:09 +0000

    remove zzz.R containing onload stuff

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-#' Initialise the package including detect environment and load default paths
-.onLoad<-function(libname=find.package('BTOTools'),pkgname='BTOTools') {
-  cat("BTOTools version 0.1.0 - access and manipulate BTO data")
-  if(.Platform$OS.type =='windows') assign('.BTOarchive_path','//btodomain/FILES/UNIXArchive/',1)
-  if(.Platform$OS.type=='unix') assign('.BTOarchive_path','/archive/',1)
-}
