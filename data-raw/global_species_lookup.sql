242dffda4e8a3edede527b9cedcab4b59f615cd9
Simon Gillings <simon.gillings@bto.org>
2020-06-10 09:48:16 +0100

    new sql to include Euring code in the global_species_lookup

--- data-raw/global_species_lookup.sql
+++ data-raw/global_species_lookup.sql
@@ -1,7 +1,7 @@
 select b.master_taxon_id,b.scientific_name,nvl(lsa.bou_name,b.english_name) ENGLISH_NAME,
 b.taxon_rank_id,ltr.TAXON_RANK_NAME, b.sort_order,
 case when lsa.bou_name is not null then b.english_name else null end ALT_INT_NAME,
-lsa.CBC_CODE,lsa.FIVE_LETTER
+lsa.CBC_CODE,lsa.FIVE_LETTER,lsa.euring_no
 from species.baselist_taxa b left join species.l_species_aux lsa on b.master_taxon_id=lsa.master_taxon_id
 inner join L_TAXON_RANK ltr on ltr.TAXON_RANK_ID=b.TAXON_RANK_ID
 where b.baselist_abbrev='IOC_BTO'
