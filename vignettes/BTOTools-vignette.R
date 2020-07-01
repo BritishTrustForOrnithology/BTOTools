2179144cff8d696b87262e439b9db01c7f06071c
Simon Gillings <simon.gillings@bto.org>
2020-07-01 15:32:34 +0100

    vignette updating

---
+++ vignettes/BTOTools-vignette.R
@@ -0,0 +1,20 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- fig.show='hold'----------------------------------------------------
+library(BTOTools)
+temp1<-data.frame(cbc_code='R.', 
+                  count=10,
+                  onekm='TL1234', 
+                  stringsAsFactors = FALSE)
+rescale_1km_to_2km(temp1,'onekm')
+rescale_1km_to_10km(temp1,'onekm')
+
+## ---- fig.show='hold'----------------------------------------------------
+library(BTOTools)
+data("centroids050")
+plot(centroids050$easting, centroids050$northing, asp = 1)
+
