119de4b677c9fe3c88dc6f482d4236dadd507158
Philipp Boersch-Supan <philipp.boerschsupan@bto.org>
2021-10-05 14:55:56 +0100

    Revert "Merge branch 'master' into master"
    This reverts commit b6373b0923942de41e7fa9f500db3e8481873b8a, reversing
    changes made to 1f62192604a0f54ba17254897499ec0fbd8d3402.

--- R/rescale_1km_to_10km.R
+++ R/rescale_1km_to_10km.R
@@ -35,34 +35,3 @@ rescale_1km_to_10km<-function(df,invar) {
   df$tenkm<-paste(substr(df[,invar.index],1,3),substr(df[,invar.index],5,5),sep=''  )
   return(df)
 }
-
-#' Convert a 1-km grid reference to a 10-km grid reference
-#'
-#' @description
-#' Takes a vector of British National grid 1-km references and converts to 10-km references.
-#' Works for Britain, Ireland and Channel Isles grids
-#'
-#' @param x character vector containing the input grid reference
-#'
-#' @return A character vector with tenkm references
-#' 
-#' @examples
-#' temp1<-data.frame(cbc_code='R.',count=10,onekm='TL1234', stringsAsFactors = FALSE)
-#' rescale_1km_to_10km_(temp1$onekm)
-#'
-#' @export
-#'
-rescale_1km_to_10km_<-function(x) {
-  #check input parameters
-  if(!is.character(x)) stop('invar must be supplied as a character string')
-  
-  #check if any values in the column are not 1-km refs
-  nc <- nchar(x)
-  short <- min(nc, na.rm = TRUE)
-  long <- max(nc, na.rm = TRUE)
-  if(short < 6) stop('x contains grid references that are too short')
-  if(long > 6) stop('x contains grid references that are too long')
-  
-  tenkm<-paste(substr(x,1,3),substr(x,5,5),sep=''  )
-  return(tenkm)
-}
