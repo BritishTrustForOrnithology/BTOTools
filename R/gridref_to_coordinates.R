5e52769a16aff48db06a5a8330896a115b91a13e
Simon Gillings <simon.gillings@bto.org>
2021-12-29 11:01:53 +0000

    extended to cover Irish and Channel Islands grid references

--- R/gridref_to_coordinates.R
+++ R/gridref_to_coordinates.R
@@ -1,18 +1,25 @@
-#' Converts a column of British National grid references to columns of eastings and northings
+#' Converts a column of grid references to columns of eastings and northings
 #'
 #' @description
-#' Takes a dataframe of British National grid reference and produce new columns of eastings and 
-#' northings. Only works for Britain.
+#' Takes a dataframe of grid reference and produce new columns of eastings and 
+#' northings. This function will work for British, Irish and Channel Islands grid references. 
+#' But note that it does not translate between regions. i.e. Channel Islands grid references 
+#' are converted to Channel Islands coordinates; Irish grid references are converted to Irish
+#' coordinates. If you apply this function to a dataframe containing grid references from 
+#' multiple regions it will return coordinates for multiple regions.
 #'
 #' @param df name of input dataframe
 #' @param invar_gridref quoted string name of the variable containing the grid reference
 #'
-#' @return The same dataframe with additional easting and northing columns.
+#' @return The same dataframe with additional easting and northing columns and region 
+#' column indicating which grid system each row originates from, and therefore which grid
+#' system the coordinates are from.
 #'
 #'
 #' @examples
 #' temp1 <- data.frame(other1 = 'A', other2 = 'B', 
-#' square = c('TL1', 'TL12', 'TL12A', 'TL1234', 'TL123456', 'TL1234567', 'GB1234', 'TL12O'), 
+#' square = c('TL1', 'TL12', 'TL12A', 'TL1234', 'TL123456', 'TL1234567', 'GB1234', 
+#' 'TL12O', 'IN1234', 'WV2575'), 
 #' stringsAsFactors = FALSE)
 #' temp1a<-gridref_to_coordinates(temp1,'square')
 #'
@@ -22,7 +29,6 @@ gridref_to_coordinates<-function(df, invar_gridref) {
   #check input parameters
   if(!is.data.frame(df)) stop('df must be a data frame')
   if(!is.character(invar_gridref)) stop('invar_gridref must be character strings')
-  
   
   #check there aren't already easting and northing columns
   if('easting' %in% names(df)) stop('df already contains a column called easting')
@@ -34,17 +40,12 @@ gridref_to_coordinates<-function(df, invar_gridref) {
   #force name to be gridref to make easier processing
   names(temp_df)<- c('gridref')
   
-  #give warning if any Irish or Channel Isles squares
-  lets <- unique(substr(temp_df$gridref,1,1))
-  if("I" %in% lets) warning('df contains Irish grid references. These will not be processed')
-  if("W" %in% lets) warning('df contains Channel Isles grid references. These will not be processed')
-  
   #make columns ready
   temp_df$easting <- NA
   temp_df$northing <- NA
-  
+
   #add a sortorder so can return in original order
-  temp_df$order <- row.names(temp_df)
+  temp_df$order <- as.numeric(row.names(temp_df))
   
   #check the resolution of the gridrefs - it may vary and could contain unprocessable strings
   temp_df$nchar <- nchar(temp_df$gridref)
@@ -74,17 +75,36 @@ gridref_to_coordinates<-function(df, invar_gridref) {
                 "NR","NS","NT","NU","NL","NM","NN",
                 "NO","NF","NG","NH","NJ","NK",
                 "NA","NB","NC","ND","HY","HZ","HT",
-                "HU","HP"),
+                "HU","HP",
+                "WA", "WV",
+                "IA", "IB", "IC", "ID",
+                "IF", "IG", "IH", "IJ",
+                "IL", "IM", "IN", "IO",
+                "IQ", "IR", "IS", "IT",
+                "IV", "IW", "IX", "IY"),
     e1000 = c(0L,1L,2L,3L,4L,5L,1L,
               2L,3L,4L,5L,6L,1L,2L,3L,4L,5L,6L,2L,3L,4L,
               5L,6L,2L,3L,4L,5L,1L,2L,3L,4L,1L,2L,3L,
               4L,0L,1L,2L,3L,0L,1L,2L,3L,4L,0L,1L,2L,3L,
-              3L,4L,3L,4L,4L),
+              3L,4L,3L,4L,4L,
+              5L, 5L,
+              0L, 1L, 2L, 3L,
+              0L, 1L, 2L, 3L,
+              0L, 1L, 2L, 3L,
+              0L, 1L, 2L, 3L,
+              0L, 1L, 2L, 3L),
     n1000 = c(0L,0L,0L,0L,0L,0L,1L,
               1L,1L,1L,1L,1L,2L,2L,2L,2L,2L,2L,3L,3L,3L,
               3L,3L,4L,4L,4L,4L,5L,5L,5L,5L,6L,6L,6L,
               6L,7L,7L,7L,7L,8L,8L,8L,8L,8L,9L,9L,9L,9L,
-              10L,10L,11L,11L,12L)
+              10L,10L,11L,11L,12L,
+              55L,54L,
+              4L, 4L, 4L, 4L,
+              3L, 3L, 3L, 3L,
+              2L, 2L, 2L, 2L,
+              1L, 1L, 1L, 1L, 
+              0L, 0L, 0L, 0L),
+    region = c(rep('GB', 53), rep('CH', 2), rep('I', 20))
   )
   
   lookup_tetlet <- data.frame(
@@ -197,6 +217,7 @@ gridref_to_coordinates<-function(df, invar_gridref) {
   }
   
   #join them all back together
+  temp_df_errors$region <- NA
   temp_done <- rbind(temp_df_errors, temp_df4, temp_df5, temp_df6, temp_df8)
   temp_done <- temp_done[order(temp_done$order),]
   temp_done$order <- NULL
@@ -206,6 +227,11 @@ gridref_to_coordinates<-function(df, invar_gridref) {
   #check if any NAs returned and send warning
   if(anyNA(temp_done$easting) | anyNA(temp_done$northing)) 
     warning('NAs were returned; indicates at least one record could not be converted and may indicate error in grid reference')
+
+  #check if more than one grid system being used
+  regions <- unique(temp_done$region[!is.na(temp_done$region)])
+  if(length(regions)>1)
+    warning(paste0('More than one grid system represented in these data: ', paste(unlist(regions), collapse = ',')))
   
   #check df and temp_df still same length
   if(nrow(df) != nrow(temp_done)) stop('processed list not same length as original df')
